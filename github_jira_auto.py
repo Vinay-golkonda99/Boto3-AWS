c707f8158e23fcf60be1ebc586876459333a109a
Vinay-golkonda99 <122544244+Vinay-golkonda99@users.noreply.github.com>
2024-07-22 16:23:59 -0400

    Update github_jira_auto.py

--- github_jira_auto.py
+++ github_jira_auto.py
@@ -1,20 +1,16 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Define a route that handles GET requests
+# Define a route that handles POST requests
 @app.route('/createJira', methods=['POST'])
 def createJira():
+    url = "https://vinayprasadg.atlassian.net/rest/api/3/issue"
 
-    
-    url = "https://vinayprasadg.atlassian.net/rest/api/3/project"
-
-    API_TOKEN="ATATT3xFfGF0rkd6LUx0UQlHLNUMpdUjRNlC0EXTqcL4SrURd4OSGiEteO6CTK1n4Mr5RLtsHgjOQoWzr8553TzJsEFSqit3KutPFZPwSJHrP54K1lhMv5zeHb4htthrueXNWvfskNu08YH4YrodKCcFah0KwYj1-BoFULrCMaUVCBN6hLFzJBk=01863F66"
+    API_TOKEN = "ATATT3xFfGF0rkd6LUx0UQlHLNUMpdUjRNlC0EXTqcL4SrURd4OSGiEteO6CTK1n4Mr5RLtsHgjOQoWzr8553TzJsEFSqit3KutPFZPwSJHrP54K1lhMv5zeHb4htthrueXNWvfskNu08YH4YrodKCcFah0KwYj1-BoFULrCMaUVCBN6hLFzJBk=01863F66"
 
     auth = HTTPBasicAuth("gvinayprasad12@gmail.com", API_TOKEN)
 
@@ -23,34 +19,33 @@ def createJira():
         "Content-Type": "application/json"
     }
 
-    payload = json.dumps( {
+    payload = json.dumps({
         "fields": {
-        "description": {
-            "content": [
-                {
-                    "content": [
-                        {
-                            "text": "Order entry fails when selecting supplier.",
-                            "type": "text"
-                        }
-                    ],
-                    "type": "paragraph"
+            "description": {
+                "content": [
+                    {
+                        "content": [
+                            {
+                                "text": "Order entry fails when selecting supplier.",
+                                "type": "text"
+                            }
+                        ],
+                        "type": "paragraph"
                     }
                 ],
-            "type": "doc",
-             "version": 1
+                "type": "doc",
+                "version": 1
+            },
+            "project": {
+                "key": "VIN"
+            },
+            "issuetype": {
+                "id": "10003"
+            },
+            "summary": "Main order flow broken",
         },
-        "project": {
-           "key": "VIN"
-        },
-        "issuetype": {
-            "id": "10003"
-        },
-        "summary": "Main order flow broken",
-    },
-    "update": {}
-    } )
-
+        "update": {}
+    })
 
     response = requests.request(
         "POST",
@@ -62,3 +57,5 @@ def createJira():
 
     return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
 
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
